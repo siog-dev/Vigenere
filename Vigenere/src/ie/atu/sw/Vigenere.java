85eae772c3c48ebe779918bd12366fcfb3a99f90
siog <siog@LAPTOP-545TJ6PE>
2025-06-02 17:14:06 +0100

    Create a for loop that appends the decrypted chars to a string

--- Vigenere/src/ie/atu/sw/Vigenere.java
+++ Vigenere/src/ie/atu/sw/Vigenere.java
@@ -32,7 +32,11 @@ public class Vigenere {
 	}
 	
 	public String decrypt(String cipherText) {
-		return null;
+		
+		for (int i = 0; i < cipherText.length(); i++) {
+			sb.append(getDecryptedCharacter(key[i], cipherText.charAt(i)));
+		}
+		return sb.toString;
 	}
 	
 	private char getDecryptedCharacter(char key, char cipher) {
