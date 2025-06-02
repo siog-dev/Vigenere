00e8b1a465134b72abe818afeac2ef57f3291fd7
siog <siog@LAPTOP-545TJ6PE>
2025-06-02 16:32:23 +0100

    Encrypt plainText and assign it to a new string called cipherText

--- Vigenere/src/ie/atu/sw/Runner.java
+++ Vigenere/src/ie/atu/sw/Runner.java
@@ -7,5 +7,6 @@ public class Runner {
 		String plainText = "ATTACK THE CASTLE WALL AT DAWN"; //Cryptography version of "Hello, world!"
 		
 		Vigenere cipher = new Vigenere();
+		String cipherText = cipher.encrypt(plainText);
 	}
 }
