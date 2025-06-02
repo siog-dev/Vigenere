48d66ac23dcd7a441536c9bc0aaac9a08b5d92c9
siog <siog@LAPTOP-545TJ6PE>
2025-06-02 16:16:09 +0100

    Create method stubs/signatures returning null

--- Vigenere/src/ie/atu/sw/Vigenere.java
+++ Vigenere/src/ie/atu/sw/Vigenere.java
@@ -2,4 +2,12 @@ package ie.atu.sw;
 
 public class Vigenere {
 
+	public String encrypt(String plainText) {
+		return null;
+	}
+	
+	public String decrypt(String cipherText) {
+		return null;
+	}
+	
 }
