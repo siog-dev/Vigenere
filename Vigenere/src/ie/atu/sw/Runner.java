f238506b39e76bf4d548991d97472fd095380b6c
siog <siog@LAPTOP-545TJ6PE>
2025-06-02 16:26:37 +0100

    Create a new instance of the Vigenere class

--- Vigenere/src/ie/atu/sw/Runner.java
+++ Vigenere/src/ie/atu/sw/Runner.java
@@ -5,5 +5,7 @@ public class Runner {
 	public static void main(String[] args) {
 		String key = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
 		String plainText = "ATTACK THE CASTLE WALL AT DAWN"; //Cryptography version of "Hello, world!"
+		
+		Vigenere cipher = new Vigenere();
 	}
 }
