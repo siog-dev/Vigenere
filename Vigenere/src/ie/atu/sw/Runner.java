0a4178ebf51cfebd09ce0337726d12a4d0ba65e8
siog <siog@LAPTOP-545TJ6PE>
2025-06-08 18:42:05 +0100

    Add a try/catch block to deal with exception in runner

--- Vigenere/src/ie/atu/sw/Runner.java
+++ Vigenere/src/ie/atu/sw/Runner.java
@@ -4,11 +4,15 @@ public class Runner {
 
 	public static void main(String[] args) {
 		String key = "THEQUICK";
-		String plainText = "ATTACK THE CASTLE WALL AT DAWN"; 
+		String plainText = "ATTACK THE CASTLE WALL AT DAWN";
 		
-		Vigenere cipher = new Vigenere(key);
-		String cipherText = cipher.encrypt(plainText);
-		System.out.println(cipherText);
-		System.out.println(cipher.decrypt(cipherText));
+		try {
+			Vigenere cipher = new Vigenere(key);
+			String cipherText = cipher.encrypt(plainText);
+			System.out.println(cipherText);
+			System.out.println(cipher.decrypt(cipherText));
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
