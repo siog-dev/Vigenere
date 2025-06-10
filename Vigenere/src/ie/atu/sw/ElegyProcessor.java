094a5454f9bff94468fc882f3857cc5bfe454cef
siog <siog@LAPTOP-545TJ6PE>
2025-06-10 13:16:51 +0100

    Declare file, add bones of try/catch method

--- Vigenere/src/ie/atu/sw/ElegyProcessor.java
+++ Vigenere/src/ie/atu/sw/ElegyProcessor.java
@@ -1,5 +1,21 @@
 package ie.atu.sw;
 
+import java.io.*;
+
 public class ElegyProcessor {
 
+	// Put in the Java I/O Code
+	public static void main(String[] args) {
+		/* ./ is more specific - works cross platform
+		 * don't hardcode any filepaths (C:// etc)
+		 */
+		String file = "./elegy.txt";
+		
+		try {
+			Vigenere v = new Vigenere("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG");
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
