9449f81fdb866cb0087bc927dcfcd8755230bc65
siog <siog@LAPTOP-545TJ6PE>
2025-06-10 13:37:07 +0100

    Read line by line, encrypt each line and write each line to a file

--- Vigenere/src/ie/atu/sw/ElegyProcessor.java
+++ Vigenere/src/ie/atu/sw/ElegyProcessor.java
@@ -9,11 +9,38 @@ public class ElegyProcessor {
 		/* ./ is more specific - works cross platform
 		 * don't hardcode any filepaths (C:// etc)
 		 */
-		String file = "./elegy.txt";
+		String file = "./elegy.txt"; // point at our file
 		
 		try {
-			Vigenere v = new Vigenere("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG");
+			Vigenere v = new Vigenere("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG"); //Here's our key
+			/*
+			 * Read through the elegy line by line
+			 * encrypt each line and write each line to a file
+			 */
+			FileWriter out = new FileWriter("out.txt"); // Write stuff out to this + CLOSE
 			
+			/*
+			 * (file) = file
+			 * FileInputStream = InputStream, Read a byte. From what? a File.
+			 * Get each byte from the file and do what?
+			 * InputStreamReader = Convert each byte into a character.
+			 * And then do what?
+			 * BufferedReader = Buffer it - add it to a buffer
+			 * + CLOSE
+			 */
+			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+			String line = null; // Declare a local variable to represent each line
+			
+			// Loop line by line until null/EOL/End of Line, assign each line to line
+			while ((line = br.readLine()) != null) {
+				// Encrpyt it
+				if (line.length() > Vigenere.MIN_KEY_SIZE) { // If the line is greater than min
+					out.write(v.encrypt(line.trim().toUpperCase()) + "\n"); // Then encrypt that line
+				}
+			}
+			
+			br.close();
+			out.close(); // Close to prevent resource leak
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
