229f759a80b03d20898c8a8675eae938f92ef0d8
siog <siog@LAPTOP-545TJ6PE>
2025-06-10 13:59:40 +0100

    Decrypt out.txt to out1.txt

--- Vigenere/src/ie/atu/sw/ElegyProcessor.java
+++ Vigenere/src/ie/atu/sw/ElegyProcessor.java
@@ -5,18 +5,18 @@ import java.io.*;
 public class ElegyProcessor {
 
 	public static void main(String[] args) {
-		String file = "./elegy.txt";
+		String file = "./out.txt";
 		
 		try {
 			Vigenere v = new Vigenere("THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG");
-			FileWriter out = new FileWriter("out.txt");
+			FileWriter out = new FileWriter("out1.txt");
 			
 			BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
 			
 			String line = null;
 			while ((line = br.readLine()) != null) {
 				if (line.length() > Vigenere.MIN_KEY_SIZE) {
-					out.write(v.encrypt(line.trim().toUpperCase()) + "\n");
+					out.write(v.decrypt(line.trim().toUpperCase()) + "\n");
 				}
 			}
 			
